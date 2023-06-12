e5538b032fc0464a82d88c283a6951ecfc5f7fd0
HAKIZIMANA Franck <hakifranckmana@gmail.com>
2023-06-12 21:01:15 +0200

    utiliser les variables d'environnement dans migrations-db.php

--- migrations-db.php
+++ migrations-db.php
@@ -1,9 +1,11 @@
 <?php
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 return [
-    'dbname' => 'gestionstage',
-    'user' => 'root',
-    'password' => 'root',
-    'host' => 'localhost',
+    'dbname' => $_ENV["NOM_BASE_DE_DONNEE"],
+    'user' => $_ENV["NOM_UTILISATEUR_BD"],
+    'password' => $_ENV["MOT_DE_PASS_UTILISATEUR_BD"],
+    'host' => $_ENV["ADDRESSE_MACHINE_HOST_BD"],
     'driver' => 'pdo_mysql',
 ];
